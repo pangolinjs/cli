9224da02eb6f6dec1ef74f8378a5bb23b1fa3ccb
Fynn Becker <info@fynn-becker.de>
2018-10-12 11:42:18 +0200

    Remove AVA test runner from template

--- template/src/functions/object-from-array.unit.js
+++
@@ -1,9 +0,0 @@
-import objectFromArray from './object-from-array'
-import test from 'ava'
-
-test('objectFromArray', t => {
-  const result = objectFromArray(['hello', 'world'])
-  const expect = { name: 'hello', value: 'world' }
-
-  t.deepEqual(result, expect)
-})
